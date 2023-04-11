6e2116eb9cb7f9ec2f5b7852a87238d469cc401f
Kamil Sikora <avler455@gmail.com>
2023-04-11 21:59:34 +0200

    start-002

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import  ReactDOM  from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import React from "react"
+import App from "./App"
+import "./main.scss"
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+
+  
+
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
   </React.StrictMode>
 )
