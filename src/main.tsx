5418d81dc0c123664d3bd0437e2578b4a2731807
Kamil Sikora <avler455@gmail.com>
2023-05-31 23:32:09 +0200

    ver-032

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-
 import App from "./App";
-import dataReducer from "./redux/dataSlice";
+import dataReducer from "./Redux/dataSlice";
 
 const store = configureStore({
   reducer: {
