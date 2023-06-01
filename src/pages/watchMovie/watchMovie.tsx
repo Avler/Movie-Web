7eaa7bf9a105fea8ad1a6cc0e6f6d256311aaa39
Kamil Sikora <avler455@gmail.com>
2023-06-01 22:00:31 +0200

    ver-058

--- src/pages/watchMovie/watchMovie.tsx
+++ src/pages/watchMovie/watchMovie.tsx
@@ -4,9 +4,9 @@ import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
 import { dataMovies } from "../../App";
-import VideoContainer from "../../Components/Youtube/VideoConteiner";
-import MoviesList from "../../Components/MoviesList/MoviesList";
-import TvShowsList from "../../Components/TvShowList/TvShowsList";
+import VideoContainer from "../../components/Youtube/VideoConteiner";
+import MoviesList from "../../components/MoviesList/MoviesList";
+import TvShowsList from "../../components/TvShowList/TvShowsList";
 import "../../commonStyle/stylePages.scss";
 
 const watchMovie = ()=>{
